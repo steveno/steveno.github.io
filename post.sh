03c5733627cd46fedf2f7287503dc7d4cacaafb1
steveno <oliver.steven@gmail.com>
2024-03-02 18:09:24 -0500

    Add script to start new post

---
+++ post.sh
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#!/bin/bash
+
+# Get current date, year, and month
+DATE=$(date +"%Y-%m-%d")
+YEAR=$(date +"%Y")
+MONTH=$(date +"%m")
+
+# Get title from user input
+read -rp "Post title: " TITLE
+read -rp "Enter post slug:" SLUG
+
+# Create year and month directories if they don't exist
+mkdir -p _posts/"$YEAR"/"$MONTH"
+
+# Create the file with YAML frontmatter
+cat > _posts/"$YEAR"/"$MONTH"/"$DATE"-"$SLUG".md <<EOL
+---
+
+* TOC
+{:toc}
+
+title: "$TITLE"
+date: $DATE
+categories:
+- 
+keywords: 
+description: ""
+---
+
+## 
+
+EOL
